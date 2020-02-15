e5d8805adec58956d3a8c58c1312b6afbe641c08
giftyinyang <caomiehinyang@gmail.com>
2020-02-15 21:54:46 +0100

    my new commit

--- javascript/app.js
+++ javascript/app.js
@@ -22,3 +22,36 @@ function closeMenu() {
     hamburger.style.display = 'block'
 
 }
+
+
+///DISPLAY TESTIMONIALS ON CLICK OF THE CIRCLES
+
+const circleOne = document.querySelector('.circle-one').addEventListener('click', displayOne)
+const circleTwo = document.querySelector('.circle-two').addEventListener('click', displayTwo)
+const circleThree = document.querySelector('.circle-three').addEventListener('click', displayThree)
+const circleFour = document.querySelector('.circle-four').addEventListener('click', displayFour)
+const testItemOne = document.querySelector('.test-item-one')
+const testItemTwo = document.querySelector('.test-item-two')
+const testItemThree = document.querySelector('.test-item-three')
+const testItemFour = document.querySelector('.test-item-four')
+
+
+
+function displayOne() {
+    testItemOne.classList.add('display-testimonial')
+
+    //remove from other testimonials
+    testItemTwo.classList.remove('display-testimonial')
+    testItemThree.classList.remove('display-testimonial')
+    testItemFour.classList.remove('display-testimonial')
+}
+
+function displayTwo() {
+    testItemTwo.classList.add('display-testimonial')
+
+    testItemOne.classList.remove('display-testimonial')
+    testItemThree.classList.remove('display-testimonial')
+    testItemFour.classList.remove('display-testimonial')
+}
+
+
